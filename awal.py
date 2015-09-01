93535196b5df4afb9e3ee4d12f0f663eca878a6e
DeaDania <deadania27@gmail.com>
2015-09-01 15:18:07 +0700

    first commit

---
+++ awal.py
@@ -0,0 +1,5 @@
+print "selamat datang "
+nama = raw_input ("masukkan nama anda :")
+umur = input("masukkan umur anda:")
+print "hai {0} , umur anda {1} ya".format(nama, umur)
+
---
+++ luas_segitiga.py
@@ -0,0 +1,7 @@
+print " Selamat datang di program penghitung luas segitiga"
+print " INPUT :"
+alas = input (" Alas  :")
+tinggi = input (" Tinggi :")
+luas = alas * tinggi /2
+print " Luas Segitiga adalah {}".format(luas)
+
